a2dce0e3670dd9bb43c919371dbd62faed9b29f4
Mona <tweinim7@gmail.com>
2021-08-31 17:40:35 -0700

    Let's get it!

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -10,8 +10,23 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
+        int maxNumber = 40, previousNumber = 0, nextNumber = 1;
+        System.out.print("Fibonacci Series of "+maxNumber+" numbers:");
+
+        int i=1;
+        while(i <= maxNumber)
+        {
+            System.out.print(previousNumber+" ");
+            int sum = previousNumber + nextNumber;
+            previousNumber = nextNumber;
+            nextNumber = sum;
+            i++;
+        }
 
     }
 
-
 }
+
+
+
+
