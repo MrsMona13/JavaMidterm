2aaa1b80524e76ba89b5a078d6c815fd4519a3cc
Mona <tweinim7@gmail.com>
2021-08-31 19:40:37 -0700

    Let's get it!

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -10,15 +10,15 @@ public class CollectionView {
          Retrieve the Collection view of the values present in map
          */
 
-        
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+        HashMap<Integer, String> map = new HashMap<>();
         map.put(1, "NYC");
         map.put(2, "LA");
         map.put(3, "Denver");
         map.put(4, "Boston");
         map.put(5, "San Jose");
         map.put(6, "Seattle");
-
+        System.out.println(map);
     }
 
 }
