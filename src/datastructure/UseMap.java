b5a22c07b1fce60e6ddbe77d1bc7dfa11971a832
Mona <tweinim7@gmail.com>
2021-08-31 21:43:37 -0700

    Let's do this!

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
     public static void main(String[] args) {
@@ -11,6 +13,19 @@ public class UseMap {
          * Store and retrieve data from/to a database table.
          */
 
-    }
+        Map<String, Integer> items = new HashMap<>();
+        items.put("shoes", 3);
+        items.put("clothing", 2);
+        items.put("accessories", 1);
 
+        Iterator<Map.Entry<String, Integer>> it = items.entrySet().iterator();
+
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) ((Iterator<?>) it).next();
+
+            System.out.format("items: %s, quantity: %d%n", pair.getKey(),
+                    pair.getValue());
+        }
+    }
 }
+
